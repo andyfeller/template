ce67ee2912fd6d43e6492c3595a5a337dc5ad5cb
Andy Feller <andyfeller@github.com>
2023-07-29 01:52:08 -0400

    Fixing base script, adding codeowners

--- bin/00-base.sh
+++ bin/00-base.sh
@@ -4,9 +4,10 @@
 source $PUBLICIZE_LIB/helpers.sh
 
 # Ensure base files every repository needs are there if missing; do not override
-copyMissingFile $GPM_SOURCE_DIR ".gitignore"
-copyMissingFile $GPM_SOURCE_DIR "CODE_OF_CONDUCT.md"
-copyMissingFile $GPM_SOURCE_DIR "LICENSE.txt"
+copyMissingFile $PUBLICIZE_SOURCE_DIR ".gitignore"
+copyMissingFile $PUBLICIZE_SOURCE_DIR "CODEOWNERS"
+copyMissingFile $PUBLICIZE_SOURCE_DIR "CODE_OF_CONDUCT.md"
+copyMissingFile $PUBLICIZE_SOURCE_DIR "LICENSE.txt"
 
 # Update repository labels as appropriate
 updateLabels
